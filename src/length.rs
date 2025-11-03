95cf259d619344f24b9bd1245a28fb35b32633b9
Shizuko Nakano <shizukommunist@gmail.com>
2025-11-03 18:14:57 -0300

    update on main.rs and creating length.rs

---
+++ src/length.rs
@@ -0,0 +1,7 @@
+pub fn conversion() {
+    println!("\nWhich unit set to convert from?\n(1) metric (2) imperial");
+    println!("\nWhich unit to convert from?\n(1) meters  (2) kilometers  (3)  centimeters");
+    println!("\nInput your unit:");
+    println!("\nWhich unit set to convert to?\n(1) metric  (2) imperial");
+    println!("\nWhich unit to convert to?\n(1) feet  (2) miles  (3) yards");
+}
